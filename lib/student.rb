38d5b4bf25235b3f2261b8882aad73d7bbebaed0
Garrett Thompson <gtcruz.gt@gmail.com>
2020-01-17 05:52:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,20 +23,20 @@ class Student
     end
   end
 
-  def self.find_by_name(name)
+  #def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
-    sql = <<-SQL
-      SELECT * FROM students WHERE name = ?
-    SQL
+    #sql = <<-SQL
+      #SELECT * FROM students WHERE name = ?
+    #SQL
 
-    DB[:conn].execute(sql, name)
+    #DB[:conn].execute(sql, name)
     #DB[:conn].execute(sql)
 
     #sql = "SELECT * from students where name = #{name}"
 
     #DB[:conn].execute(sql)
-  end
+  #end
 
   def self.all_students_in_grade_9
     #no argument
