2943c1e1a28ac23a7256c5c52f9c8dd414a4e155
Garrett Thompson <gtcruz.gt@gmail.com>
2020-01-17 05:40:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,7 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-      SELECT * FROM students WHERE names = ?
+      SELECT * FROM students WHERE name = ?
     SQL
 
     DB[:conn].execute(sql, name)
